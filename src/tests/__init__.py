29dee1271d8a19c0125766bbf54e97cb9ca0be89
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2025-12-04 12:27:29 -0800

    Question 1 set up

--- src/tests/__init__.py
+++ src/tests/__init__.py
@@ -1,6 +1,29 @@
 import pandas as pd
 import numpy as np
-import gdown
+from datasets import load_dataset
+
+class Question1:
+    df = load_dataset("DJ-Research/codingtest", subset="question1", split="df").to_pandas()    
+    other_df = load_dataset("DJ-Research/codingtest", subset="question1", split="other_df").to_pandas()
+
+    @staticmethod
+    def random_numbers(x):
+        if not isinstance(x, int):
+            raise ValueError("Input must be an integer.")
+        if x <= 5:
+            raise ValueError("Input must be a positive integer greater than 5")
+        return np.random.randint(-x, x+1)
+    
+    @staticmethod
+    def other_random_numbers(x):
+        if not isinstance(x, int):
+            raise ValueError("Input must be an integer.")
+        if x <= 10:
+            raise ValueError("Input must be a positive integer greater than 10")
+        return np.random.randint(x)
+    
+
+
 
 
 
